fc48f91ac7044ce92c1a290933479d4d7af82ccf
nummer <37844134+the-nummer-zone@users.noreply.github.com>
2025-05-16 22:21:55 -0600

    Create 50-disable-session-suspend.lua

---
+++ .config/wireplumber/main.lua.d/50-disable-session-suspend.lua
@@ -0,0 +1,15 @@
+table.insert (alsa_monitor.rules, {
+  matches = {
+    {
+      -- Matches all sources.
+      { "node.name", "matches", "alsa_input.*" },
+    },
+    {
+      -- Matches all sinks.
+      { "node.name", "matches", "alsa_output.*" },
+    },
+  },
+  apply_properties = {
+    ["session.suspend-timeout-seconds"] = 86400,  -- 0 disables suspend - on some systems this won't work and you need to set a high value like 86400 (1 day) instead
+  },
+})
